61e469cd0ad7caf0b25b9193c0fba2afebbcea10
Dario Hu <dariohu1@icloud.com>
2022-12-20 17:03:39 +0100

    aggiorno file

--- feedback-page.js
+++ feedback-page.js
@@ -1,16 +1,35 @@
-const starsDiv = document.getElementsByClassName("stars");
-let currentValue = 0;
-
 const stars = document.querySelectorAll('.star')
-stars.forEach(star => {
-    star.addEventListener('mouseover', () => {
 
-    });
-    star.addEventListener('click', (event) => {
-        currentValue = event.target.getAttribute('data-value');
+stars.forEach(star => {
+    star.addEventListener('click', (e) => {
+    const value = e.target.getAttribute('data-value');
+    console.log(`Value: ${value}`);
     });
 });
-    
-    
 
-    
+
+
+
+
+
+
+
+// const ratingStars =[...document.getElementsByClassName("star")]
+// function executeRating(stars){
+//     const starClassActive = stars.classList.toggle(".selected")
+//     const starClassInactive="star"
+//     const starsLength = stars.length
+//     let i;
+
+// stars.map((star) => {
+//     star.onclick = () => {
+//        i = stars.indexOf(star);
+
+//        if (star.className===starClassInactive) {        
+//           for (i; i >= 0; --i) stars[i].className = starClassActive;
+//        } else {
+//           for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+//        }
+//     };
+//  });
+// }
