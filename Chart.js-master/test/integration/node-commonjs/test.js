2473fb8a6351c999d58f2466228f76d7f8dd15a6
Tommaso Nannicini <tnannicini@gmail.com>
2022-12-21 09:54:49 +0100

    Cancellato files Results Page

--- Chart.js-master/test/integration/node-commonjs/test.js
+++
@@ -1,7 +0,0 @@
-const {Chart} = require('chart.js');
-const {valueOrDefault} = require('chart.js/helpers');
-
-Chart.register({
-  id: 'TEST_PLUGIN',
-  dummyValue: valueOrDefault(0, 1)
-});
--- Chart.js-master/test/types/plugins/plugin.filler/fill_target_true.ts
+++
@@ -1,6 +0,0 @@
-import type { ChartDataset } from '../../../../src/types.js';
-
-const dataset: ChartDataset = {
-  data: [],
-  fill: true,
-};
