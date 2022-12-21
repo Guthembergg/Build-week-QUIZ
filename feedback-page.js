8f9c43ca9638e0d13e4c47cac16931ffdf476b51
Dario Hu <dariohu1@icloud.com>
2022-12-21 20:29:22 +0100

    correzione css WP e rosolto problema stelle

--- feedback-page.js
+++ feedback-page.js
@@ -4,32 +4,10 @@ stars.forEach(star => {
     star.addEventListener('click', (e) => {
     const value = e.target.getAttribute('data-value');
     console.log(`Value: ${value}`);
+    
+    for (let i = 0; i < value; i++) {
+        stars[i].classList.add('selected');
+    }
     });
 });
 
-
-
-
-
-
-
-
-// const ratingStars =[...document.getElementsByClassName("star")]
-// function executeRating(stars){
-//     const starClassActive = stars.classList.toggle(".selected")
-//     const starClassInactive="star"
-//     const starsLength = stars.length
-//     let i;
-
-// stars.map((star) => {
-//     star.onclick = () => {
-//        i = stars.indexOf(star);
-
-//        if (star.className===starClassInactive) {        
-//           for (i; i >= 0; --i) stars[i].className = starClassActive;
-//        } else {
-//           for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
-//        }
-//     };
-//  });
-// }
